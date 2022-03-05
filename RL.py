7d3d6268a339b2a97f043426f0658a8122ea3b5a
tarunaygr <60793650+tarunaygr@users.noreply.github.com>
2022-03-05 15:19:52 +0530

    Added flatten

--- RL.py
+++ RL.py
@@ -19,21 +19,20 @@ rules_list,rule_names=m3.return_rules()
     
     
 def convert(state):
-    converted=np.zeros((8,56),dtype=np.int8)
+    converted=np.zeros((8*56),dtype=np.int8)
     for i,j in enumerate(state):
-        converted[i][j]=1
+        converted[i*56+j]=1
     return converted
 
 def convert_8(state):
-    res=[]
-    for s in state:
-        res.append(s.index(1))
+    r=np.where(state==1)[0]
+    res=[s%56 for s in r]
     return res
 
 model = Sequential()
-model.add(Dense(56, input_shape=(8,56), activation='relu'))
-model.add(Dense(20, activation='relu'))
-model.add(Dense(1, activation='relu'))
+model.add(Dense(8, input_shape=(448,),batch_size=1,name='inputlayer'))
+model.add(Dense(20, activation='relu',name='hiddenlayer'))
+model.add(Dense(1, activation='relu',name='outputlayer'))
 model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 model.summary()
 average_reward=0
@@ -43,7 +42,10 @@ alpha=0.05
 gamma=.8
 # rules=[m3.rule_178,m3.rule_92,m3.rule_154,m3.rule_18,m3.rule_68,m3.rule_172,m3.rule_222,m3.rule_46]
 S=[29, 53, 42, 36, 20, 52, 6, 16]
-A=(randint(0,7),randint(0,55))
+A_val=randint(0,55)
+while(A_val in S):
+    A_val=randint(0,55)
+A=(randint(0,7),A_val)
 
 while True:
     print(f'Current State: {S}')
@@ -52,7 +54,11 @@ while True:
     R=sbox.state_crypto_strength(S_prime)
     epsilon = random()
     if epsilon<epsilon_threshold:
-        A_prime=(randint(0,7),randint(0,55))
+        print("taken epsilon")
+        rand_action=randint(0,55)
+        while(rand_action in S_prime):
+            rand_action=randint(0,55)
+        A_prime=(randint(0,7),rand_action)
         taken_state=S_prime.copy()
         taken_state[A_prime[0]]=A_prime[1]
         taken_state=convert(taken_state)
@@ -60,21 +66,31 @@ while True:
         max_states=[]
         max_actions=[]
         max_value=-inf
+        # for i in range(8):
+        #     for j in range(56):
+        #         if j in S_prime:
+        #             continue
+        #         next_state=np.zeros((8*56),dtype=np.int8)
+        #         for k in S_prime:
+        #             if k==S_prime[i]:
+        #                 continue
+        #             next_state[i*56+k]=1
+        #         next_state[i*56+j]=1
+        
         for i in range(8):
+            current_action=S_prime[i]
+            
             for j in range(56):
-                if j in S_prime:
+                if j==current_action:
                     continue
-                next_state=np.zeros((8,56),dtype=np.int8)
-                for k in S_prime:
-                    if k==S_prime[i]:
-                        continue
-                    next_state[i][k]=1
-                next_state[i][j]=1
-                print('input')
-                print(np.shape(np.array([next_state])))
+                next_state=convert(S_prime)
+                next_state[i*56+current_action]=0
+                next_state[i*56+j]=1
+                # print('input')
+                # print(np.array([np.array(next_state).flatten()]))
                 prediction=model.predict(np.array([next_state]))
-                print("prediction")
-                print(prediction)
+                # print("prediction")
+                # print(prediction)
                 if prediction>max_value:
                     max_value=prediction
                     max_states=[next_state]
@@ -88,7 +104,7 @@ while True:
     print("State")
     print(taken_state)
     Q_s_a=model.predict(np.array([convert(S_prime)]))+sbox.state_crypto_strength(S_prime)
-    Q_sprime_aprime=model.predict(np.array(taken_state))+sbox.state_crypto_strength(convert_8(taken_state))
+    Q_sprime_aprime=model.predict(np.array([taken_state]))+sbox.state_crypto_strength(convert_8(taken_state))
     target=Q_s_a+alpha*(R-average_reward+gamma*(Q_sprime_aprime)-Q_s_a)
     print(np.array([target]))
     print(np.array([convert(S_prime)]))
