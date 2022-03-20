1a1ee44b6c5cad0a357f7d01fcd6d1047f5408b5
tarunaygr <60793650+tarunaygr@users.noreply.github.com>
2022-03-20 17:34:26 +0530

    runs

--- Auxiliary/s_box.py
+++ Auxiliary/s_box.py
@@ -6,28 +6,28 @@ import itertools
 import math
 from . import funcs_filtered_outputs_m_3 as m3
 
-NUM_RULES=3
-INPUT_SIZE=8
-OUTPUT_SIZE=8
+NUM_RULES = 3
+INPUT_SIZE = 8
+OUTPUT_SIZE = 8
 
-# import Auxiliary.funcs_filtered_outputs_m_4 as m4
-# import Auxiliary.funcs_filtered_outputs_m_5 as m5
-# import Auxiliary.funcs_filtered_outputs_m_6 as m6
-
-#Helper Functions
+# Helper Functions
 
 # Convert a decimal number into a corresponding array of bit values
-def decimalToBinary(n): 
-    return "{0:09b}".format(int(n))
-  
-# Convert the binary representation array into the corresponding decimal value
-def BinaryTodecimal(bit_list):
-  dec=0
-  for bit in bit_list:
-    dec=(dec<<1)|bit
-  return dec
 
-# Function which runs the CA rule over the CA and xors the rule outputs and returns a single bit value 
+
+def decimalToBinary(n):
+    return "{0:09b}".format(int(n))
+
+# Convert the binary representation array into the corresponding decimal value
+
+
+def BinaryTodecimal(bit_list):
+    dec = 0
+    for bit in bit_list:
+        dec = (dec << 1) | bit
+    return dec
+
+# Function which runs the CA rule over the CA and xors the rule outputs and returns a single bit value
 # Parameters:
 # rule - the list of CA rules to be run on the CA
 # nb_size - Size of the neighbourhood considered
@@ -35,39 +35,23 @@ def BinaryTodecimal(bit_list):
 # ca  - The input CA configuration to run the rules on
 # Returns:
 # Output bit after running the rule and XORing the results
-# def rule_op(rule,nb_size,ca_len,ca):
-#   ops=[]
-#   for i in range(len(ca)-nb_size+1):
-#     nbr=[]
-#     for j in range(nb_size):
-#       nbr.append(ca[i+j])
-#   ops.append(rule(nbr))
-#   res=0
-#   for op in ops:
-#     res^=op
-#   return res
 
 
-def rule_op(rule,nb_size,ca_len,ca,start):
-  ops=[]
-  for i in range(start,start+ca_len-nb_size+1):
-    nbr=[]
-    for j in range(nb_size):
-      #print("i: ",i,"j: ",j)  
-      nbr.append(ca[(i+j)%ca_len])
-    #print("input: ",ca)
-    #print("neighbour: ",nbr)
-    #print("output = ", rule(nbr))
-    ops.append(rule(nbr))
-    res=0
-  for op in ops:
-    res^=op
-  return res
+def rule_op(rule, nb_size, ca_len, ca, start):
+    ops = []
+    for i in range(start, start+ca_len-nb_size+1):
+        nbr = []
+        for j in range(nb_size):
+            nbr.append(ca[(i+j) % ca_len])
+        ops.append(rule(nbr))
+        res = 0
+    for op in ops:
+        res ^= op
+    return res
 
-#CA Rules Definitions which we run on the Cellular Automata. These are rules of neighbourhood size 3
-rule_list,rule_names= m3.return_rules()
-# rules=[m3.rule_178,m3.rule_92,m3.rule_154,m3.rule_18,m3.rule_68,m3.rule_172,m3.rule_222,m3.rule_46]
-# rules=rule_list[:8]
+
+# CA Rules Definitions which we run on the Cellular Automata. These are rules of neighbourhood size 3
+rule_list, rule_names = m3.return_rules()
 
 # Function to emulate the output of the S-box. It outputs the inputs and corresponding outputs in a bit list form.
 # Parameters:
@@ -76,42 +60,24 @@ rule_list,rule_names= m3.return_rules()
 # inputs - All possible inputs tried in the s-box
 # outputs - Outputs for the correspoding values of input
 
-# def Sbox(rules):
-#   inputs=[]
-#   outputs=[]
-#   for i in range(256):
-#     res = list(map(int, str(decimalToBinary(i))))
- 
-#     inputs.append(res[1:])
-#     op=[]
-#     for rule in rules:
-#       op.append(rule_op(rule,3,8,res))
-#     outputs.append(op)
-#     # print(f'{res[1:]} ->{op}')
-#   return inputs,outputs
-
-
 def Sbox(rules):
-    inputs=[]
-    outputs=[]
-    
+    inputs = []
+    outputs = []
+
     for i in range(2**INPUT_SIZE):
-        #print("--------------------------------new ip------------------------------")
         res = list(map(int, str(decimalToBinary(i))))
         res = res[1:]
         inputs.append(res)
-        op=[]
+        op = []
         start = 0
         for rule in rules:
-            #print("-------------new rule---------------")
             for i in range(math.ceil(INPUT_SIZE/NUM_RULES)):
-                
-                op.append(rule_op(rule,3,INPUT_SIZE,res,start))
-                #print("one bit added for start", start)
-                start +=1
+
+                op.append(rule_op(rule, 3, INPUT_SIZE, res, start))
+                start += 1
         op = op[0:8]
         outputs.append(op)
-    return inputs,outputs
+    return inputs, outputs
 
 
 # Function to check the bijectivity of the s-box function. Returns 1 if bijective, 0 if not
@@ -120,14 +86,14 @@ def Sbox(rules):
 # Return:
 # 1 if the s-box is bijective. 0 otherwise
 def bijectivity(decimal_repr):
-  len_ops=len(decimal_repr)
-  len_distinct=len(set(decimal_repr))
-  if (len_ops==len_distinct):
-    print("It is Bijective")
-    return 1
-  else:
-    print("Not Bijective")
-    return 0
+    len_ops = len(decimal_repr)
+    len_distinct = len(set(decimal_repr))
+    if (len_ops == len_distinct):
+        print("It is Bijective")
+        return 1
+    else:
+        print("Not Bijective")
+        return 0
 
 
 # Function to Calculate the Difference Distribution Table of the S-box function and returns its differential uniformity
@@ -136,133 +102,112 @@ def bijectivity(decimal_repr):
 # Returns:
 # The maximum value in the DDT ( the differential uniformity of the s-box)
 def diff_uniformity(decimal_repr):
-  ddt=np.zeros((256,256))
-  for a in range(256):
-    for x in range(256):
-      sum=x^a
-      F1=decimal_repr[sum]
-      F2=decimal_repr[x]
-      b=F1^F2
-      ddt[a][b]+=1
-  for i in range(2**INPUT_SIZE):
-    ddt[i][i]=0
-  ddt[0]=np.zeros(2**INPUT_SIZE)
-  return (np.amax(ddt))
+    ddt = np.zeros((256, 256))
+    for a in range(256):
+        for x in range(256):
+            sum = x ^ a
+            F1 = decimal_repr[sum]
+            F2 = decimal_repr[x]
+            b = F1 ^ F2
+            ddt[a][b] += 1
+    # for i in range(2**INPUT_SIZE):
+    #     ddt[i][i] = 0
+    ddt[0] = np.zeros(2**INPUT_SIZE)
+    return (np.amax(ddt))
 
-def innerprod(a,x):
-  res=0
-  if (len(x)!=len(a)):
-    print(f"SIZE a={len(a)} SIZE x={len(x)}")
-  for i in range(len(a)):
-    res^=((a[i]*x[i])%2)
-  return res
 
-#The function to calcute the WHT of the s-box for given values of u,v
+def innerprod(a, x):
+    res = 0
+    if (len(x) != len(a)):
+        print(f"SIZE a={len(a)} SIZE x={len(x)}")
+    for i in range(len(a)):
+        res ^= ((a[i]*x[i]) % 2)
+    return res
+
+# The function to calcute the WHT of the s-box for given values of u,v
 # Parameters:
 # u,v
 # inarray - 2D array with all possible inputs to the s-box
-# outarray - 2D array with the corresponding outputs for the inputs 
+# outarray - 2D array with the corresponding outputs for the inputs
 # Returns:
-# WHT value 
-def WHT_Calc(u,v,inarray,outarray):
-  WHT=0
-  for i in range(len(inarray)):
-    x=innerprod(v,outarray[i])
-    y=innerprod(u,inarray[i])
-    WHT+=pow(-1,x^y)
-  # print(f'u={u} v={v} WHT={WHT}')
-  return WHT
+# WHT value
+
+
+def WHT_Calc(u, v, inarray, outarray):
+    WHT = 0
+    for i in range(len(inarray)):
+        x = innerprod(v, outarray[i])
+        y = innerprod(u, inarray[i])
+        WHT += pow(-1, x ^ y)
+    return WHT
 
 # Function to find the max value of WHT over all the values of u,v
 # Parameters:
 # inarray - 2D array with all possible inputs to the s-box
-# outarray - 2D array with the corresponding outputs for the inputs 
+# outarray - 2D array with the corresponding outputs for the inputs
 # Returns:
 # the maximum value of WHT
 
-# def get_WHT_spectrum(inarray,outarray):
-  
-#   input_binaries=[]
-#   for i in range(256):
-#     input_binaries.append(list(map(int, str(decimalToBinary(i)))))
-#   v_vals = input_binaries.copy()[1:]
-#   u_vals = input_binaries.copy()
-#   # v_vals=v_vals[1:]
-#   # u_vals=u_vals[1:]
-#   max=-inf
-#   for u in u_vals:
-#     for v in v_vals:
-#       WHT_curr=abs(WHT_Calc(u,v,inarray,outarray))
-#       if (WHT_curr>max):
-#         # print(f'U={u} V={v} WHY ={WHT_curr}')
-#         max=WHT_curr
-#   return max
-      
-def get_WHT_spectrum(inarray,outarray):
-  v_vals = list(itertools.product([0, 1], repeat=OUTPUT_SIZE))
-  u_vals = list(itertools.product([0, 1], repeat=INPUT_SIZE))
-  v_vals=v_vals[1:]
-  # u_vals=u_vals[1:]
-  max=-inf
-  for u in u_vals:
-    for v in v_vals:
-      WHT_curr=abs(WHT_Calc(u,v,inarray,outarray))
-      if (WHT_curr>max):
-        # print(f'U={u} V={v} WHY ={WHT_curr}')
-        max=WHT_curr
-  return max
-      
+def get_WHT_spectrum(inarray, outarray):
+    v_vals = list(itertools.product([0, 1], repeat=OUTPUT_SIZE))
+    u_vals = list(itertools.product([0, 1], repeat=INPUT_SIZE))
+    v_vals = v_vals[1:]
+    max = -inf
+    for u in u_vals:
+        for v in v_vals:
+            WHT_curr = abs(WHT_Calc(u, v, inarray, outarray))
+            if (WHT_curr > max):
+                max = WHT_curr
+    return max
+
 # Function to Calculate the Nonlinearity of the s-box
-# Parameters: 
+# Parameters:
 # inarray - 2D array with all possible inputs to the s-box
-# outarray - 2D array with the corresponding outputs for the inputs 
+# outarray - 2D array with the corresponding outputs for the inputs
 # n - The size of each input (8 here)
 # Returns;
 # Nonlinearity of the s-box
-def NLcalc(inarray,outarray,n):
-  wht=get_WHT_spectrum(inarray,outarray)
-  # print(f'wht={wht}')
-  NL = pow(2,n-1) - wht/2
-  return NL
+
+
+def NLcalc(inarray, outarray, n):
+    wht = get_WHT_spectrum(inarray, outarray)
+    NL = pow(2, n-1) - wht/2
+    return NL
 
 # Function to calculate the cryptographic strength of an s-box.
 # Parameters:
 # rules -  the set of rules to run the sbox
 # Returns:
 # The strength of the s-box according to our formulation
-def state_crypto_strength(rules_index):
-  # print("Current State Checking Strength")
-  # print(rules_index)
-  rules=[ rule_list[i]  for i in rules_index]
-  inarray,outarray = Sbox(rules)
-  # print("INARRAY")
-  # for i in inarray:
-  #   if(len(i)!=8):
-  #     print(i)
-  # print("OUTARRAY")
-  # for i in outarray:
-  #   if(len(i)!=8):
-  #     print(i)
-  print(len(outarray[1]))
-  print("Hello")
-  decimal_repr=[]
-  for bit_list in outarray:
-    decimal_repr.append(BinaryTodecimal(bit_list))
-  DU = diff_uniformity(decimal_repr)
-  NL = NLcalc(inarray,outarray,INPUT_SIZE)
-  print(f"DU = {str(DU)} NL={str(NL)}")
-  normalised_NL = (NL/112)*100
-  DU1 = ((DU-4)/(128-4))*100
-  normalised_DU = 100 - DU1
-  reward = (normalised_NL + normalised_DU)/2
-  
-  return reward
-  
+
+
+def state_crypto_strength(rules_index, to_print):
+
+    rules = [rule_list[i] for i in rules_index]
+    inarray, outarray = Sbox(rules)
+    decimal_repr = []
+    for bit_list in outarray:
+        decimal_repr.append(BinaryTodecimal(bit_list))
+    DU = diff_uniformity(decimal_repr)
+    NL = NLcalc(inarray, outarray, INPUT_SIZE)
+
+    normalised_NL = (NL/112)*100
+    DU1 = ((DU-4)/(128-4))*100
+    normalised_DU = 100 - DU1
+    reward = (normalised_NL + normalised_DU)/2
+    if to_print:
+        print(f"DU = {str(DU)} NL={str(NL)}")
+        print(f"Strength: {round(reward,2)}")
+
+    return reward, DU, NL
+
 # Function to calculate the immediate reward for a particular state transition.
 # Parameters:
 # rule1 - the initial state of the s-box
 # rule2 - the final state of the s-box
 # Returns:
 # The immediate reward achieved by the transition
+
+
 def state_transition_reward(rule1, rule2):
-  return state_crypto_strength(rule2) - state_crypto_strength(rule1)
+    return state_crypto_strength(rule2) - state_crypto_strength(rule1)
--- RL.py
+++ RL.py
@@ -1,125 +1,127 @@
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras import Sequential
+import keras
+import numpy as np
+import Auxiliary.funcs_filtered_outputs_m_3 as m3
 from cmath import inf
 from cv2 import threshold
+from sympy import false
 import Auxiliary.s_box as sbox
 import os
-from random import random,randint,choice
+from random import random, randint, choice
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import Auxiliary.funcs_filtered_outputs_m_3 as m3
-import numpy as np
-#from keras.models import Sequential
-import keras
-#from keras.layers import Dense
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import  Flatten, Dense
 
-NUM_RULES=2
-INPUT_SIZE=8
-OUTPUT_SIZE=8
+NUM_RULES = 3
+INPUT_SIZE = 8
+OUTPUT_SIZE = 8
+
 
 def convert(state):
-    converted=np.zeros((NUM_RULES*56),dtype=np.int8)
-    for i,j in enumerate(state):
-        converted[i*56+j]=1
+    converted = np.zeros((NUM_RULES*56), dtype=np.int8)
+    for i, j in enumerate(state):
+        converted[i*56+j] = 1
     return converted
 
+
 def convert_8(state):
-    r=np.where(state==1)[0]
-    res=[s%56 for s in r]
+    r = np.where(state == 1)[0]
+    res = [s % 56 for s in r]
     return res
 
-rules_list,rule_names=m3.return_rules()
+
+rules_list, rule_names = m3.return_rules()
 
 model = Sequential()
-model.add(Dense(56*NUM_RULES, input_shape=(56*NUM_RULES,),batch_size=1,name='inputlayer'))
-model.add(Dense(56, activation='relu',name='hiddenlayer'))
-model.add(Dense(1, activation='relu',name='outputlayer'))
+model.add(Dense(56*NUM_RULES, input_shape=(56*NUM_RULES,),
+          batch_size=1, name='inputlayer'))
+model.add(Dense(56, activation='relu', name='hiddenlayer'))
+model.add(Dense(1, activation='relu', name='outputlayer'))
 model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 # model.summary()
-average_reward=0
-beta=0.1
-epsilon_threshold=0.4
-alpha=0.05
-gamma=.8
+average_reward = 0
+beta = 0.1
+epsilon_threshold = 0.4
+alpha = 0.05
+gamma = .8
+best_strength = -inf
+best_DU = -inf
+best_NL = -inf
+best_states = []
+visited_states = []
 # rules=[m3.rule_178,m3.rule_92,m3.rule_154,m3.rule_18,m3.rule_68,m3.rule_172,m3.rule_222,m3.rule_46]
-S=[29, 53]
-A_val=randint(0,55)
-while(A_val in S):
-    A_val=randint(0,55)
-A=(randint(0,NUM_RULES-1),A_val)
+S = [29, 53, 23]
+A_val = randint(0, 55)
+# while(A_val in S):
+#     A_val=randint(0,55)
+A = (randint(0, NUM_RULES-1), A_val)
 print(f'Initial State: {[rule_names[s] for s in S]}')
-while True:
+try:
+    while True:
 
-    print(f"Replacing {rule_names[S[A[0]]]} with {rule_names[A[1]]}...")
-    
-    S_prime=S.copy()
-    S_prime[A[0]]=A[1]
-    print(f'Current State: {[rule_names[s] for s in S_prime]}')
-    R=sbox.state_crypto_strength(S_prime)
-    epsilon = random()
-    if epsilon<epsilon_threshold:
-        print("Exploring...")
-        rand_action=randint(0,55)
-        while(rand_action in S_prime):
-            rand_action=randint(0,55)
-        A_prime=(randint(0,NUM_RULES-1),rand_action)
-        taken_state=S_prime.copy()
-        taken_state[A_prime[0]]=A_prime[1]
-        taken_state=convert(taken_state)
-    else:
-        print("Exploiting...")
-        max_states=[]
-        max_actions=[]
-        max_value=-inf
-        # for i in range(8):
-        #     for j in range(56):
-        #         if j in S_prime:
-        #             continue
-        #         next_state=np.zeros((8*56),dtype=np.int8)
-        #         for k in S_prime:
-        #             if k==S_prime[i]:
-        #                 continue
-        #             next_state[i*56+k]=1
-        #         next_state[i*56+j]=1
-        
-        for i in range(NUM_RULES):
-            current_action=S_prime[i]
-            
-            for j in range(56):
-                if j==current_action:
-                    continue
-                next_state=convert(S_prime)
-                next_state[i*56+current_action]=0
-                next_state[i*56+j]=1
-                # print('input')
-                # print(np.array([np.array(next_state).flatten()]))
-                prediction=model.predict(np.array([next_state]))
-                # print("prediction")
-                # print(prediction)
-                if prediction>max_value:
-                    max_value=prediction
-                    max_states=[next_state]
-                    max_actions=[(i,j)]
-                elif prediction==max_value:
-                    max_states.append(next_state)
-                    max_actions.append((i,j))
-        action_index=randint(0,len(max_actions)-1)
-        taken_state=max_states[action_index]
-        A_prime=max_actions[action_index]
-    # print("State")
-    # print(taken_state)
-    Q_s_a=model.predict(np.array([convert(S_prime)]))+sbox.state_crypto_strength(S_prime)
-    Q_sprime_aprime=model.predict(np.array([taken_state]))+sbox.state_crypto_strength(convert_8(taken_state))
-    target=Q_s_a+alpha*(R-average_reward+gamma*(Q_sprime_aprime)-Q_s_a)
-    # print(np.array([target]))
-    # print(np.array([convert(S_prime)]))
-    model.fit(np.array([convert(S_prime)])
-                ,np.array([target]))
-    delta=R-average_reward+Q_sprime_aprime-Q_s_a
-    average_reward+=beta*delta
-    S=S_prime
-    A=A_prime
-        
-                    
-                
-    
-    
+        print(f"Replacing {rule_names[S[A[0]]]} with {rule_names[A[1]]}...")
+
+        S_prime = S.copy()
+        S_prime[A[0]] = A[1]
+        print(f'Current State: {[rule_names[s] for s in S_prime]}')
+        if S_prime not in visited_states:
+            visited_states.append(S_prime)
+        R, DU, NL = sbox.state_crypto_strength(S_prime, True)
+        if(R > best_strength):
+            best_strength = R
+            best_DU = DU
+            best_NL = NL
+            best_states = [[rule_names[s] for s in S_prime]]
+        elif (R == best_strength):
+            best_states.append([rule_names[s] for s in S_prime])
+        epsilon = random()
+        if epsilon < epsilon_threshold:
+            print("Exploring...")
+            rand_action = randint(0, 55)
+            while(rand_action in S_prime):
+                rand_action = randint(0, 55)
+            A_prime = (randint(0, NUM_RULES-1), rand_action)
+            taken_state = S_prime.copy()
+            taken_state[A_prime[0]] = A_prime[1]
+            taken_state = convert(taken_state)
+        else:
+            print("Exploiting...")
+            max_states = []
+            max_actions = []
+            max_value = -inf
+            for i in range(NUM_RULES):
+                current_action = S_prime[i]
+
+                for j in range(56):
+                    if j == current_action:
+                        continue
+                    next_state = convert(S_prime)
+                    next_state[i*56+current_action] = 0
+                    next_state[i*56+j] = 1
+                    prediction = model.predict(np.array([next_state]))
+                    if prediction > max_value:
+                        max_value = prediction
+                        max_states = [next_state]
+                        max_actions = [(i, j)]
+                    elif prediction == max_value:
+                        max_states.append(next_state)
+                        max_actions.append((i, j))
+            action_index = randint(0, len(max_actions)-1)
+            taken_state = max_states[action_index]
+            A_prime = max_actions[action_index]
+        Q_s_a = model.predict(np.array([convert(S_prime)]))+R
+        Q_sprime_aprime = model.predict(np.array(
+            [taken_state]))+sbox.state_crypto_strength(convert_8(taken_state), False)[0]
+        target = Q_s_a+alpha*(R-average_reward+gamma*(Q_sprime_aprime)-Q_s_a)
+        model.fit(np.array([convert(S_prime)]), np.array([target]))
+        delta = R-average_reward+Q_sprime_aprime-Q_s_a
+        average_reward += beta*delta
+        S = S_prime
+        A = A_prime
+except KeyboardInterrupt:
+    print("Exiting the RL...\n")
+    print("FINAL REPORT:")
+    print(f'Number of States Visited: {len(visited_states)}')
+    print(f"Best Strength: {round(best_strength,2)}")
+    print(f'Best DU: {best_DU} Best NL: {best_NL}')
+    print('Best States:')
+    print(best_states)
